5c79eff9ad54de7d187ae986521f6c906ed77be9
Nelson Osvaldo Salinas Guajardo <ns4lin4s@gmail.com>
2017-01-27 02:07:26 -0300

    Agregar npm run build al package

---
+++ src/client/js/index.js
@@ -0,0 +1,4 @@
+var button = document.getElementById('record')
+button.onclick = function(){
+    alert('perfect')
+}
---
+++ src/server/server.js
@@ -0,0 +1,35 @@
+
+'use strict'
+
+const Hapi = require('hapi')
+
+const port = process.env.PORT || 8080
+
+const server = new Hapi.Server()
+
+server.connection({
+
+	host: 'localhost',
+	port: 8080
+
+})
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Hello, world!')
+    }
+})
+
+server.start(function(err){
+
+    if (err) {
+       throw err;
+    }
+    console.log('Server running at:', server.info.uri);
+})
+
+
+
+
