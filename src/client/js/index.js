2be1496a93f400d877d982f7ca5b596ed85d770f
Nelson Osvaldo Salinas Guajardo <ns4lin4s@gmail.com>
2017-01-29 00:32:04 -0300

    Capturar webcam e inicializarla en un div

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -1,4 +1,24 @@
+var Webrtc2images = require('webrtc2images')
+var rtc2images = new Webrtc2images({
+      width: 320,
+      height: 320,
+      frames: 20,
+      type: 'image/jpeg',
+      quality: 1,
+      interval: 100
+
+});
+
+rtc2images.startVideo(function (err) {
+    if (err) {
+            console.log(err);
+
+    }
+})
+
 var button = document.getElementById('record')
 button.onclick = function(){
-    alert('perfect')
+    rtc2images.recordVideo(function(err, frames){
+        console.log(frames)
+    })
 }
